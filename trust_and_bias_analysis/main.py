b877c93aae373c4af6d76b70f597beade39434ae
Radek Tomasek <radek.tomasek@gmail.com>
2025-05-17 08:30:04 -0700

    Adjusted the input param name

--- trust_and_bias_analysis/main.py
+++ trust_and_bias_analysis/main.py
@@ -14,7 +14,7 @@ def run():
     """
     parser = argparse.ArgumentParser(description='Run Ground News Crew Analysis')
     parser.add_argument(
-        '--input_article',
+        '--article',
         type=str,
         help='Input article for the analysis'
     )
@@ -22,11 +22,11 @@ def run():
     args = parser.parse_args()
 
     inputs = {
-        "input_article": args.input_article
+        "input_article": args.article
     }
 
     try:
-        print(f"Starting data analysis for Ground News with article= {args.input_article}")
+        print(f"Starting data analysis for Ground News with article= {args.article}")
         crew_result = GroundNewsCrew(inputs=inputs).crew().kickoff()
         print("Text Analysis completed successfully")
         print("\n\n########################")
